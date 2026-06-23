475baf4ceb0902a88a8f50cce743b3cda788af4f
PrabhudattaPatra <135804634+PrabhudattaPatra@users.noreply.github.com>
2026-06-23 07:53:02 +0530

    Time: 2 ms (99.35%), Space: 20.6 MB (22.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,6 @@
+import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-       i = 0
-       j = len(s) - 1
-
-       while i < j:
-            if not s[i].isalnum():
-                i += 1
-                continue
-            if not s[j].isalnum():
-                j -= 1
-                continue
-            
-            if s[i].lower() != s[j].lower():
-                return False
-                
-            i += 1
-            j -= 1
-       return True
+       x = re.sub(r'[^a-zA-Z0-9]','',s)
+       x = x.lower()
+       return x == x[::-1]
