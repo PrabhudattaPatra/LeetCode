ba4ac5379a11c57717e52fb3b16010fbf27bab43
PrabhudattaPatra <135804634+PrabhudattaPatra@users.noreply.github.com>
2026-06-26 09:46:54 +0530

    Time: 34 ms (27.65%), Space: 28.7 MB (96.09%) - LeetHub

--- LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,14 +1,26 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        k = 10
+        if len(s)<k:
+            return []
         seen = set()
         result = set()
-
-        for i in range(len(s) - 9):
-            string = s[i:i+10]
-
-            if string in seen:
-                result.add(string)
-            else:
-                seen.add(string)
-
+        rep = 0
+        # 0123456789
+        # AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT
+        # 9876543210
+        dic = {'A':0,'C':1,'G':2,'T':3}
+        for i in range(k):
+            power = k - i - 1
+            rep += (4**power)*dic[s[i]]
+        seen.add(rep)
+        for i in range(k,len(s)):
+            rep -= (4**(k-1))*dic[s[i-k]]
+            rep *= 4
+            rep += dic[s[i]]
+            if rep in seen:
+                result.add(s[i-k+1:i+1])
+            seen.add(rep)
         return list(result)
+
+       
